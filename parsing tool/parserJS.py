c25a110e1cd476c4847efb421baa4de24f8042ff
Akash <akashjais9341@gmail.com>
2026-01-27 11:58:42 +0530

    javascript parser tested

---
+++ parsing tool/parserJS.py
@@ -0,0 +1,73 @@
+from tree_sitter import Parser
+from tree_sitter_languages import get_language
+
+class JSFunctionChunker:
+    def __init__(self):
+        self.language = get_language("javascript")
+        self.parser = Parser()
+        self.parser.set_language(self.language)
+
+    def parse(self, code: str):
+        src = code.encode("utf8")
+        tree = self.parser.parse(src)
+        return tree, src
+
+    def extract(self, node, src):
+        return src[node.start_byte:node.end_byte].decode()
+
+    def collect(self, node, src, out):
+        # Standard function declaration:  function foo() {}
+        if node.type == "function_declaration":
+            out.append(self.extract(node, src))
+
+        # Function expression: const foo = function(){}
+        if node.type == "function":
+            out.append(self.extract(node, src))
+
+        # Arrow function: const foo = () => {}
+        if node.type == "arrow_function":
+            out.append(self.extract(node, src))
+
+        # Class method: class X { foo(){} }
+        if node.type == "method_definition":
+            out.append(self.extract(node, src))
+
+        # Recursively check children
+        for child in node.children:
+            self.collect(child, src, out)
+
+    def chunk(self, code: str):
+        tree, src = self.parse(code)
+        chunks = []
+        self.collect(tree.root_node, src, chunks)
+        return chunks
+
+
+# Test usage
+if __name__ == "__main__":
+    js_code = """
+function foo(x) {
+  return x + 1;
+}
+
+const bar = function(y) { 
+  return y * 2 
+}
+
+const baz = (z) => {
+  console.log(z)
+}
+
+class A {
+  hello() {
+    console.log("Hello")
+  }
+}
+"""
+
+    chunker = JSFunctionChunker()
+    chunks = chunker.chunk(js_code)
+
+    print("=== JS FUNCTION CHUNKS ===")
+    for i, c in enumerate(chunks, 1):
+        print(f"\n--- Function #{i} ---\n{c}")
